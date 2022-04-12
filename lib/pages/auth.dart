8c1e7b67610651f3f105c8f3d8a47fbf36c1ca55
Taweechai Yuenyang <krumii.it@gmail.com>
2022-04-12 12:00:15 +0700

    xxx

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 
-import '../components/background.dart';
-import 'register.dart';
-
 class AuthenticationPage extends StatefulWidget {
   const AuthenticationPage({Key? key}) : super(key: key);
 
@@ -11,95 +8,128 @@ class AuthenticationPage extends StatefulWidget {
 }
 
 class AuthenticationPageWidget extends State<AuthenticationPage> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  bool isVisible = true;
+
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-
     return Scaffold(
-      body: Background(
+      backgroundColor: Colors.red.shade300,
+      body: Container(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+          children: [
             Container(
-              alignment: Alignment.centerLeft,
-              padding: EdgeInsets.symmetric(horizontal: 40),
-              child: Text(
-                "LOGIN",
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Color(0xFF2661FA),
-                    fontSize: 36),
-                textAlign: TextAlign.left,
+              padding: const EdgeInsets.symmetric(vertical: 50),
+              child: Image.asset(
+                'lib/assets/images/profile.png',
+                height: 200,
+                width: 200,
               ),
             ),
-            SizedBox(height: size.height * 0.03),
-            Container(
-              alignment: Alignment.center,
-              margin: EdgeInsets.symmetric(horizontal: 40),
-              child: TextField(
-                decoration: InputDecoration(labelText: "Username"),
-              ),
-            ),
-            SizedBox(height: size.height * 0.03),
-            Container(
-              alignment: Alignment.center,
-              margin: EdgeInsets.symmetric(horizontal: 40),
-              child: TextField(
-                decoration: InputDecoration(labelText: "Password"),
-                obscureText: true,
-              ),
-            ),
-            Container(
-              alignment: Alignment.centerRight,
-              margin: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
-              child: Text(
-                "Forgot your password?",
-                style: TextStyle(fontSize: 12, color: Color(0XFF2661FA)),
-              ),
-            ),
-            SizedBox(height: size.height * 0.05),
-            Container(
-              alignment: Alignment.centerRight,
-              margin: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
-              child: RaisedButton(
-                onPressed: () {},
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(80.0)),
-                textColor: Colors.white,
-                padding: const EdgeInsets.all(0),
-                child: Container(
-                  alignment: Alignment.center,
-                  height: 50.0,
-                  width: size.width * 0.5,
-                  decoration: new BoxDecoration(
-                      borderRadius: BorderRadius.circular(80.0),
-                      gradient: new LinearGradient(colors: [
-                        Color.fromARGB(255, 255, 136, 34),
-                        Color.fromARGB(255, 255, 177, 41)
-                      ])),
-                  padding: const EdgeInsets.all(0),
-                  child: Text(
-                    "LOGIN",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                ),
-              ),
-            ),
-            Container(
-              alignment: Alignment.centerRight,
-              margin: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
-              child: GestureDetector(
-                onTap: () => {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => RegisterPage()))
-                },
-                child: Text(
-                  "Don't Have an Account? Sign up",
-                  style: TextStyle(
-                      fontSize: 12,
-                      fontWeight: FontWeight.bold,
-                      color: Color(0xFF2661FA)),
+            Expanded(
+              child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 32),
+                constraints: const BoxConstraints.expand(),
+                decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(35),
+                        topRight: Radius.circular(35))),
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 50,
+                    ),
+                    const Text(
+                      'Sign Up',
+                      style:
+                          TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    TextField(
+                      controller: emailController,
+                      onChanged: (value) {
+                        setState(() {});
+                      },
+                      decoration: InputDecoration(
+                          // icon: Icon(Icons.mail),
+                          prefixIcon: const Icon(Icons.mail),
+                          suffixIcon: emailController.text.isEmpty
+                              ? const Text('')
+                              : GestureDetector(
+                                  onTap: () {
+                                    emailController.clear();
+                                  },
+                                  child: const Icon(Icons.close)),
+                          hintText: 'example@mail.com',
+                          labelText: 'Email',
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8),
+                              borderSide: const BorderSide(
+                                  color: Colors.red, width: 1))),
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    TextField(
+                      obscureText: isVisible,
+                      controller: passwordController,
+                      onChanged: (value) {
+                        print(value);
+                      },
+                      //
+                      decoration: InputDecoration(
+                          // icon: Icon(Icons.mail),
+                          prefixIcon: const Icon(Icons.lock),
+                          suffixIcon: GestureDetector(
+                              onTap: () {
+                                isVisible = !isVisible;
+                                setState(() {});
+                              },
+                              child: Icon(isVisible
+                                  ? Icons.visibility
+                                  : Icons.visibility_off)),
+                          hintText: 'type your password',
+                          labelText: 'Password',
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8),
+                              borderSide: const BorderSide(
+                                  color: Colors.red, width: 1))),
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    ElevatedButton(
+                        onPressed: () {
+                          showDialog(
+                              context: context,
+                              builder: (context) {
+                                return SimpleDialog(
+                                  title: const Text('Your submitted data '),
+                                  children: [
+                                    ListTile(
+                                      leading: const Icon(Icons.mail),
+                                      title:
+                                          Text(emailController.text.toString()),
+                                    ),
+                                    ListTile(
+                                      leading: const Icon(Icons.lock),
+                                      title: Text(
+                                          passwordController.text.toString()),
+                                    ),
+                                  ],
+                                );
+                              });
+                        },
+                        child: const Padding(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 16, vertical: 10),
+                          child: Text('Submit'),
+                        ))
+                  ],
                 ),
               ),
             )
